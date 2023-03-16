7343502d272d046675c98eb6a736702bbf9791fc
Adithya R U <112653455+areyouadithya@users.noreply.github.com>
2023-03-16 17:35:37 +0000

    done

---
+++ syscall/640buffusingcalls.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/file.h>
+#include <sys/sendfile.h>
+#include <sys/random.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+ 
+#define BUFFER_SIZE 671088640
+ 
+int main() {
+ 
+    int fOut, fIn;
+ 
+    printf("\nI/O test with sendfile() and related system calls.\n\n");
+ 
+    // Grab a BUFFER_SIZE buffer.
+    // The buffer will have random data in it but we don't care about that.
+    printf("Allocating 640 MB buffer:                     ");
+    char *buffer = (char *) malloc(BUFFER_SIZE);
+    printf("DONE\n");
+ 
+
+    // Write the buffer to fOut
+    printf("Writing data to first buffer:                ");
+    fOut = open("buffer1", O_RDONLY);
+    write(fOut, &buffer, BUFFER_SIZE);
+    close(fOut);
+    printf("DONE\n");
+ 
+    printf("Copying data from first file to second:      ");
+    fIn = open("buffer1", O_RDONLY);
+    fOut = open("buffer2", O_RDONLY);
+    sendfile(fOut, fIn, 0, BUFFER_SIZE);
+    close(fIn);
+    close(fOut);
+    printf("DONE\n");
+ 
+    printf("Freeing buffer:                              ");
+    free(buffer);
+    printf("DONE\n");
+ 
+    printf("Deleting files:                              ");
+    unlink("buffer1");
+    unlink("buffer2");
+    printf("DONE\n");
+ 
+    return 0;
+ 
+}
---
+++ syscall/640buffusingglibc.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/file.h>
+#include <sys/random.h>
+ 
+#define BUFFER_SIZE 671088640
+#define BUFFER_1 "buffer1"
+#define BUFFER_2 "buffer2"
+ 
+int main() {
+ 
+    FILE *fOut, *fIn;
+ 
+    printf("\nI/O test with traditional glibc functions.\n\n");
+ 
+    // Grab a BUFFER_SIZE buffer.
+    // The buffer will have random data in it but we don't care about that.
+    printf("Allocating 640 MB buffer:                     ");
+    char *buffer = (char *) malloc(BUFFER_SIZE);
+    printf("DONE\n");
+ 
+    // Write the buffer to fOut
+    printf("Writing data to first buffer:                ");
+    fOut = fopen(BUFFER_1, "wb");
+    fwrite(buffer, sizeof(char), BUFFER_SIZE, fOut);
+    fclose(fOut);
+    printf("DONE\n");
+ 
+    printf("Copying data from first file to second:      ");
+    fIn = fopen(BUFFER_1, "rb");
+    fOut = fopen(BUFFER_2, "wb");
+    fread(buffer, sizeof(char), BUFFER_SIZE, fIn);
+    fwrite(buffer, sizeof(char), BUFFER_SIZE, fOut);
+    fclose(fIn);
+    fclose(fOut);
+    printf("DONE\n");
+ 
+    printf("Freeing buffer:                              ");
+    free(buffer);
+    printf("DONE\n");
+ 
+    printf("Deleting files:                              ");
+    remove(BUFFER_1);
+    remove(BUFFER_2);
+    printf("DONE\n");
+ 
+    return 0;
+ 
+}
